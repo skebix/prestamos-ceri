b245729d7b30ecc024012b3d9381231649fecc23
Néstor Manuel <nestor706@gmail.com>
2016-03-01 20:13:06 -0430

    Removing application and system folders from server, and index.php from the urls.

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'system';
+	$system_path = 'C:/Users/skebix/Desktop/prestamos-ceri/system';
 
 /*
  *---------------------------------------------------------------
@@ -114,7 +114,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'application';
+	$application_folder = 'C:/Users/skebix/Desktop/prestamos-ceri/application';
 
 /*
  *---------------------------------------------------------------
