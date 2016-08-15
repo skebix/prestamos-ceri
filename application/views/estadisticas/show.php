1ce8657422ec7d66e1a3d7e7751ca9d9d66df0f1
SkyDaddy <eternal.aj@gmail.com>
2016-08-15 06:51:01 -0400

    Segunda fase de estadisticas en construccion

--- application/views/estadisticas/show.php
+++ application/views/estadisticas/show.php
@@ -7,14 +7,14 @@
  */
 ?>
 <div class="panel panel-default">
-    <div class="panel-heading">Estad&iacutesticas de uso</div>
+    <div class="panel-heading">Estad&iacute;sticas del servicio de pr&eacute;stamos</div>
 
     <table class="table table-striped table-hover ">
         <thead>
         <tr>
             <th>Tipo de usuario</th>
             <th>Cantidad de usuarios</th>
-            <th>Tiempo de pr&eacutestamos</th>
+            <th>Tiempo de pr&eacute;stamos</th>
         </tr>
         </thead>
         <tbody>
@@ -29,6 +29,32 @@
             <th scope="row">TOTALES</th>
             <th><?php echo $summary['cantidad_total']; ?></th>
             <th><?php echo $summary['tiempo_total']; ?></th>
+        </tr>
+        </tbody>
+    </table>
+</div>
+<div class="panel panel-default">
+
+    <table class="table table-striped table-hover ">
+        <thead>
+        <tr>
+            <th>Salas y espacios</th>
+            <th>Cantidad de prestamos</th>
+            <th>Tiempo de uso</th>
+        </tr>
+        </thead>
+        <tbody>
+        {materials_stats}
+        <tr>
+            <td scope="row">{espacio}</td>
+            <td>{cantidad_prestamos}</td>
+            <td>{tiempo_prestamos}</td>
+        </tr>
+        {/materials_stats}
+        <tr>
+            <th scope="row">TOTALES</th>
+            <th><?php //echo $summary2['cantidad_total']; ?></th>
+            <th><?php //echo $summary2['tiempo_total']; ?></th>
         </tr>
         </tbody>
     </table>
